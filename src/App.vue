0282eee7d434b34b8438187e7c3b67aff11f5365
rodpd <rodrigo.pad@outlook.com.br>
2022-07-21 20:09:24 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <HelloWorld/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1 class="text-7xl text-blue-400">Teste</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ['msg']
+}
+</script>
+
+<style scoped>
+
+</style>
