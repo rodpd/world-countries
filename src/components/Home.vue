786a10dadf0b74022bf41790618d621c5c7558fb
rodpd <rodrigo.pad@outlook.com.br>
2022-08-03 20:04:36 -0300

    add region filter

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,11 @@
 <template>
 
 <main class="mt-4 bg-mgray-200">
-	
-	<div class="p-6 ml-14">
+	<div class="py-6 pl-6 ml-14">
 		<SearchInput v-model="search"/>
+		<RegionFilterDropdown @update:region="(newRegion) => this.region = newRegion" />
 	</div>
+
 	
 	<div class="flex flex-wrap gap-8 px-16 py-4">
 	<CardCountry v-for="country in filteredCountries" :key="country.name.common" 
@@ -26,23 +27,23 @@
 
 import CardCountry from './CardCountry.vue'
 import SearchInput from './SearchInput.vue';
+import RegionFilterDropdown from './RegionFilterDropdown.vue';
 
 export default {
 	name: 'HelloWorld',
 	components: {
 		CardCountry,
-		SearchInput
+		SearchInput,
+		RegionFilterDropdown
 	},
 	data() {
 		return {
 			countries: [],
-			search: ''
+			search: '',
+			region: 'All'
 		}
 	},
 	methods: {
-		log(item) {
-			console.log(item)
-		},
 		fetchData() {
 			return fetch('https://restcountries.com/v3.1/all', {
 				method: 'get',
@@ -55,22 +56,21 @@ export default {
 			})
 			.then(json => {
 				this.countries = json;
-				console.log(this.countries);
 			})
 		}
 	},
 	computed: {
 		filteredCountries() {
-			return this.countries.filter((country) => country.name.common.toLowerCase().includes(this.search));
+			if (this.region == "All") {
+				return this.countries.filter((country) => country.name.common.toLowerCase().includes(this.search))
+			}
+			else {
+				return this.countries.filter((country) => country.name.common.toLowerCase().includes(this.search) && country.region == this.region)
+			}
 		}
 	},
 	mounted() {
 		this.fetchData();
-	},
-	watch: {
-		search(newSearch) {
-			console.log(newSearch);
-		}
 	}
 }
 </script>
---
+++ src/components/RegionFilterDropdown.vue
@@ -0,0 +1,49 @@
+<template>
+	<button @click="toggle" class="px-6 py-4 mr-[4vw] shadow-md outline-none bg-mgray-100 float-right rounded-lg">
+		{{region}}
+		<svg class="inline w-6 h-6 ml-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+		<path stroke-linecap="round" stroke-linejoin="round" d="M19 9l-7 7-7-7" />
+		</svg>
+	</button>
+	<ul v-show="active" class="absolute right-[4vw] mt-2">
+		<li v-for="(region, index) in regions" :key="index" 
+		@click="$emit('update:region', $event.target.innerText); toggle()"
+		class="relative border-b first:rounded-t-lg last:rounded-b-lg z-20 w-[15vw] block hover:bg-slate-100 cursor-pointer p-4 pr-10 shadow-md outline-none bg-mgray-100">
+			{{region}}
+		</li>
+	</ul>
+
+</template>
+
+
+
+<script>
+
+export default {
+	name: 'RegionFilter',
+	data () {
+		return {
+			active: false,
+			region: 'Filter by Region',
+			regions: ['All', 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+		}
+	},
+	methods: {
+		toggle() {
+			this.active = !this.active;
+		},
+		adjustListItemWidth() {
+			document.querySelectorAll("li").forEach((li) => li.style.width = document.querySelector("button").offsetWidth + "px");
+		}
+	},
+	emits: ['update:region'],
+	mounted() {
+		this.adjustListItemWidth();
+	}
+}
+</script>
+
+
+
+<style>
+</style>
